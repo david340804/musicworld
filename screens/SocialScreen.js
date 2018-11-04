a7c02160f2b32dd9f243b6f7cade243239fbd65e
haleydabbs <hdabbs3@gatech.edu>
2018-11-04 09:16:32 -0500

    Temp changes

--- screens/SocialScreen.js
+++ screens/SocialScreen.js
@@ -1,12 +1,46 @@
 import React from 'react';
-import { ScrollView, StyleSheet, Text, View, AppRegistry, Image } from 'react-native';
+import { ScrollView, StyleSheet, Text, View, AppRegistry, Image, FlatList} from 'react-native';
+import { List, ListItem } from 'react-native-elements';
 import { ExpoLinksView } from '@expo/samples';
 import Colors from '../constants/Colors.js';
 import { MusicIDButton } from '../components/MainNavButtons.js';
 
 export default class LinksScreen extends React.Component {
-  static navigationOptions = {
-    title: 'Song ID',
+  
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      loading: false,
+      data: [],
+      page: 1,
+      seed: 1,
+      error: null,
+      refreshing: false,
+    };
+  }
+
+  componentDidMount() {
+    this.makeRemoteRequest();
+  }
+
+  makeRemoteRequest = () => {
+    const { page, seed } = this.state;
+    const url = `https://randomuser.me/api/?seed=${seed}&page=${page}&results=20`;
+    this.setState({ loading: true });
+    fetch(url)
+      .then(res => res.json())
+      .then(res => {
+        this.setState({
+          data: page === 1 ? res.results : [...this.state.data, ...res.results],
+          error: res.error || null,
+          loading: false,
+          refreshing: false
+        });
+      })
+      .catch(error => {
+        this.setState({ error, loading: false });
+      });
   };
 
   render() {
@@ -14,8 +48,8 @@ export default class LinksScreen extends React.Component {
       // Try removing the `flex: 1` on the parent View.
       // The parent will not have dimensions, so the children can't expand.
       // What if you add `height: 300` instead of `flex: 1`?
-      <View style={{flex: 1}}>
-        <Text>Random users go here lol</Text>
+      <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+        <Text>Coming soon...</Text>
       </View>
     );
   }

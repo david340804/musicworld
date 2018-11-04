b272c9e86d8595c61ac8b9beeb70d81cce469856
David Gamero <david340804@gmailcom>
2018-11-04 04:15:04 -0500

    flatlist songs

--- screens/HistoryScreen.js
+++ screens/HistoryScreen.js
@@ -1,14 +1,57 @@
 import React from 'react';
 import { ExpoConfigView } from '@expo/samples';
+import { FlatList, View, Text } from 'react-native';
 
 export default class SettingsScreen extends React.Component {
   static navigationOptions = {
-    title: 'app.json',
+    title: 'Your Songs',
   };
+
+  constructor(props){
+    super(props);
+    this.state = {
+        songs: [
+            {
+                key: '1',
+                title: 'Title',
+                artist: 'Artist',
+                album: 'Album',
+            },
+            {
+                key: '2',
+                title: 'Title2',
+                artist: 'Artist2',
+                album: 'Album2',
+            },
+        ]
+    };
+  }
 
   render() {
     /* Go ahead and delete ExpoConfigView and replace it with your
      * content, we just wanted to give you a quick view of your config */
-    return <ExpoConfigView />;
+    return (
+        <FlatList
+            data={this.state.songs}
+            renderItem={
+                ({item}) => {
+                    return (
+                        <View
+                            style={{
+                                backgroundColor: 'white',
+                                marginHorizontal: 10,
+                                marginTop: 10,
+                                padding: 10,
+                            }}>
+                            <Text>{item.title}</Text>
+                            <Text>{item.artist}</Text>
+                            <Text>{item.album}</Text>
+                        </View>
+                    )
+                }
+            }
+            >
+        </FlatList>
+    );
   }
 }

35f49fced165d89c10fcd7d3d720a945b05c2f0d
jvh6 <33801471+jvh6@users.noreply.github.com>
2018-11-03 23:12:31 -0400

    Josh Push Test
    Text view added to LinkScreen

--- screens/LinksScreen.js
+++ screens/LinksScreen.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { ScrollView, StyleSheet } from 'react-native';
+import { ScrollView, StyleSheet, Text } from 'react-native';
 import { ExpoLinksView } from '@expo/samples';
 
 export default class LinksScreen extends React.Component {
@@ -10,6 +10,7 @@ export default class LinksScreen extends React.Component {
   render() {
     return (
       <ScrollView style={styles.container}>
+        <Text>{'Hi From Josh'}</Text>
         {/* Go ahead and delete ExpoLinksView and replace it with your
            * content, we just wanted to provide you with some helpful links */}
         <ExpoLinksView />
